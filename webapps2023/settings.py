04d75fec9fc46e31a4b84c6f92c4c4e3450e34c6
J Corbett <jcorb8800@gmail.com>
2023-04-22 12:40:58 +0100

    settings

--- webapps2023/settings.py
+++ webapps2023/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 SECURE_SSL_REDIRECT = env.bool('SECURE_SSL_REDIRECT')
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
