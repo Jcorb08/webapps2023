159ecdfb05de6d8813fd08d838740b95bb4d7b0a
J Corbett <jcorb8800@gmail.com>
2023-04-22 12:35:11 +0100

    settings

--- webapps2023/settings.py
+++ webapps2023/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'webapps2023.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.webapps.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.webapps.sqlite3'),
     }
 }
 
