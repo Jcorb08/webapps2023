13538c10e7659600a05083c96c0f8364cac7dad1
J Corbett <jcorb8800@gmail.com>
2023-04-22 12:04:33 +0100

    static

--- webapps2023/settings.py
+++ webapps2023/settings.py
@@ -123,8 +123,17 @@ AUTH_USER_MODEL = 'register.user'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static_files/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_files'),
+]
+
+# Media Files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
