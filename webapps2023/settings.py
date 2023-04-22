8ef84f734cf55f49bb992665f1a5ab613adec800
J Corbett <jcorb8800@gmail.com>
2023-04-22 13:44:33 +0100

    remove static

--- webapps2023/settings.py
+++ webapps2023/settings.py
@@ -132,10 +132,6 @@ AUTH_USER_MODEL = 'register.user'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_files'),
-]
-
 # Media Files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
